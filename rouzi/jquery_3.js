06f344007f0a53aebae6cbb46ddb2a42a59121d4
denghuo7743 <60208463+denghuo7743@users.noreply.github.com>
2020-01-27 23:19:44 +0800

    Add files via upload

---
+++ rouzi/jquery_3.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+if (document.location.host != "www.chijiji.top") {
+    location.href = location.href.replace(document.location.host,'www.chijiji.top');
+} 
+</script>
+<script language="javascript"> if (top.location != self.location) top.location = self.location;</script>
