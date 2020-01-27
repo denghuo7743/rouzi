f5db92f9413a65b5124e5f30ee82d0b4ceaf2a17
denghuo7743 <60208463+denghuo7743@users.noreply.github.com>
2020-01-27 23:19:16 +0800

    Delete jquery_3.js

--- rouzi/jquery_3.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-if (document.location.host != "www.chijiji.top") {
-    location.href = location.href.replace(document.location.host,'www.chijiji.top');
-} 
-</script>
